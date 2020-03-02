642702ea44a158a1c1c381740d44b6704ea15bf3
koichiro.matsuoka <majackyy@gmail.com>
2020-03-03 05:49:47 +0900

    initial commitπ

---
+++ src/test/java/com/littlehands/dddsample/DddSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littlehands.dddsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
