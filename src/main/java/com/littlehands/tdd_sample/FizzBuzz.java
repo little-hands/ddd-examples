39e0efc025d032bd6bba3b90cfba888ef2b4981f
koichiro.matsuoka <majackyy@gmail.com>
2020-05-04 07:36:02 +0900

    実行用クラス追加

--- src/main/java/com/littlehands/tdd_sample/FizzBuzz.java
+++ src/main/java/com/littlehands/tdd_sample/FizzBuzz.java
@@ -3,4 +3,13 @@ package com.littlehands.tdd_sample;
 class FizzBuzz {
   static void execute() {
   }
+
+  public static void main(String[] args) {
+    int input = Integer.parseInt(args[0]);
+    System.out.println(input);
+    FizzBuzz.execute();
+  }
 }
+
+
+
--- src/main/java/com/littlehands/tdd_sample/IssueFactory.java
+++ src/main/java/com/littlehands/tdd_sample/IssueFactory.java
@@ -3,4 +3,10 @@ package com.littlehands.tdd_sample;
 class IssueFactory {
   static void create() {
   }
+
+  public static void main(String[] args) {
+    String input = args[0];
+    System.out.println(input);
+    IssueFactory.create();
+  }
 }
