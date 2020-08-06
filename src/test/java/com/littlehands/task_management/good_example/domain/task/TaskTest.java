b1f16d375e08c1ee9a0869900513ebaac1fe291e
koichiro.matsuoka <majackyy@gmail.com>
2020-08-06 19:48:04 +0900

    feat: タスクサンプルのドメインモデル図とテストを追加

--- src/test/java/com/littlehands/task_management/good_example/domain/task/TaskTest.java
+++ src/test/java/com/littlehands/task_management/good_example/domain/task/TaskTest.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
+@SuppressWarnings("NonAsciiCharacters")
 class TaskTest {
 
   @Nested
